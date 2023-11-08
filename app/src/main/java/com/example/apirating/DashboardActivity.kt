fb98a546e7de1949fc727c1e260a1f15e55ad52a
Evelyn2323 <evelymenesesidrobo@gmail.com>
2023-11-08 13:05:01 -0500

    funcionando la api

--- app/src/main/java/com/example/apirating/DashboardActivity.kt
+++ app/src/main/java/com/example/apirating/DashboardActivity.kt
@@ -36,55 +36,6 @@ class DashboardActivity : AppCompatActivity() {
         listViewCourses = findViewById(R.id.listViewCourses)
         listViewUsers = findViewById(R.id.listViewUsers)
 
-        // Configuración de Retrofit
-        /*val retrofit = Retrofit.Builder()
-            .baseUrl("http://192.168.80.36:8000/api/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        apiService = retrofit.create(ApiService::class.java)
-
-        // Realizar la llamada a la API de cursos
-        apiService.getCursos().enqueue(object : Callback<List<cursos>> {
-            override fun onResponse(call: Call<List<cursos>>, response: Response<List<cursos>>) {
-                if (response.isSuccessful) {
-                    val courses = response.body() ?: emptyList()
-                    val coursesAdapter = ArrayAdapter(this@DashboardActivity, android.R.layout.simple_list_item_1, courses.map { it.nombre })
-                    listViewCourses.adapter = coursesAdapter
-                } else {
-                    Toast.makeText(this@DashboardActivity, "Error al cargar cursos", Toast.LENGTH_SHORT).show()
-                }
-            }
-
-            override fun onFailure(call: Call<List<cursos>>, t: Throwable) {
-                t.printStackTrace()
-                Toast.makeText(this@DashboardActivity, "Error al cargar cursos", Toast.LENGTH_SHORT).show()
-            }
-        })
-
-
-
-
-
-
-        // Realizar la llamada a la API de usuarios
-        apiService.getUsers().enqueue(object : Callback<List<users>> {
-            override fun onResponse(call: Call<List<users>>, response: Response<List<users>>) {
-                if (response.isSuccessful) {
-                    val users = response.body() ?: emptyList()
-                    val usersAdapter = ArrayAdapter(this@DashboardActivity, android.R.layout.simple_list_item_1, users.map { it.name })
-                    listViewUsers.adapter = usersAdapter
-                } else {
-                    Toast.makeText(this@DashboardActivity, "Error al cargar usuarios", Toast.LENGTH_SHORT).show()
-                }
-            }
-
-            override fun onFailure(call: Call<List<users>>, t: Throwable) {
-                t.printStackTrace()
-                Toast.makeText(this@DashboardActivity, "Error al cargar usuarios", Toast.LENGTH_SHORT).show()
-            }
-        })*/
-
 
         getCursos()
 
@@ -115,7 +66,29 @@ class DashboardActivity : AppCompatActivity() {
         }
     }
 
+    private fun getUsers() {
+        val apiService = ApiClient.getApiService()
 
+        runBlocking {
+            launch(Dispatchers.IO) {
+                try {
+                    val response = apiService.getUsers().execute()
+
+                    if (response.isSuccessful) {
+                        val users = response.body()
+                        users?.let {
+                            Log.e("USUARIOS", "$users")
+                        }
+                    } else {
+                        // Handle error
+                        Log.e("API Error", response.message())
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
 
 
 }
